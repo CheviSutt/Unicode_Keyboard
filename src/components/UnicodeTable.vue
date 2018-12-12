5d07b79d1b43dc5ef4f1dcb8947430c1198c49c2
chevisutton <cheviksutton@gmail.com>
2018-12-12 11:25:29 -0700

    Data is displaying

--- src/components/UnicodeTable.vue
+++ src/components/UnicodeTable.vue
@@ -2,19 +2,24 @@
   <div class="container">
 
     <h1>{{ title }}</h1>
-
+    <h2>{{ keys.length }}</h2>
 
     <div class="keyboard">
       <select>
         <option value="selectLanguage">Select Language</option>
       </select>
+      <table>
+        <tr v-for="key of keys">
+          <td>{{ key[4] }}</td>
+        </tr>
+      </table>
     </div>
 
   </div>
 </template>
 
 <script>
-// import { mapState } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'UnicodeTable',
@@ -23,9 +28,18 @@ export default {
       title: 'Unicode Component',
     };
   },
-
+  computed: {
+    ...mapGetters({
+      keys: 'keys',
+    }),
+  },
   methods: {
-    // ...mapState(),
+    ...mapActions([
+      'getUnicode',
+    ]),
+  },
+  created() {
+    this.getUnicode();
   },
 
 };
@@ -34,5 +48,12 @@ export default {
 <style>
   .keyboard {
     border: 1px solid black;
+  }
+  table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+  }
+  th, td {
+    padding: 5px;
   }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,23 +4,45 @@ import axios from 'axios';
 
 Vue.use(Vuex);
 
+// export const store = new Vuex.Store({
 export default new Vuex.Store({
   state: {
-    language: null,
+    keys: [],
   },
 
   mutations: {
-    updateLanguage(state, language) {
-      state.language = language;
+    updateKeys(state, keys) {
+      state.keys = keys;
+    },
+  },
+
+  getters: {
+    keys(state) {
+      return state.keys.results;// I'll want to remove results
     },
   },
 
   actions: {
     getUnicode({ commit }) {
       axios.get('http://localhost:3333/lookup?q=latin&o=0')
-        .then(result => commit('updateLanguage', result.data))
+        .then(result => commit('updateKeys', result.data))
         .catch(console.error);
     },
+
+    keyTable() {
+      const { results } = this.state.keys;// get results into a variable somehow
+      const rows = [];
+      for (let i = 0; i < results.length + 10; i += 10) {
+        const row = [];
+        for (let j = i; j < 10; j++) {
+          const td = results[j];
+          if (!td) { break; }
+          td && row.push(td);
+        }
+        rows.push(row);
+      }
+    },
+
   },
 
 });
